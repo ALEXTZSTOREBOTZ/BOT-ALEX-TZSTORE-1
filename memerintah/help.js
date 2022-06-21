3c50f6d191894d4ef6cf0025f99a73106504c2b4
ALEXTZSTOREBOTZ <107461989+ALEXTZSTOREBOTZ@users.noreply.github.com>
2022-06-21 12:26:40 +0700

    Update and rename command/help.js to memerintah/help.js

---
+++ memerintah/help.js
@@ -0,0 +1,185 @@
+const fs = require("fs");
+const petik = '```'
+let setting = JSON.parse(fs.readFileSync('./config.json'))
+const { getLimit, getBalance, cekGLimit } = require("../lib/limit")
+
+
+function toCommas(x) {
+    x = x.toString()
+    var pattern = /(-?\d+)(\d{3})/;
+     while (pattern.test(x))
+       x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+exports.allMenu = (ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isOwner, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
+    return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
+
+Hitung Mundur Idul Adha
+${mundur}
+
+*STATISTICS*
+ • Upload : ${upload}
+ • Downloads : ${download}
+
+*BOT INFO*
+ • Creator : ${ownerName}
+ • Bot Name : ${botName}
+ • Time : ${jam}
+ • Date : ${tanggal}
+ • Runtime :
+ ${runtime(process.uptime())}
+
+*USER INFO*
+ • Name : ${pushname !== undefined ? pushname : '-'}
+ • Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+ • Limit : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+ • Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
+ • Balance : $${toCommas(getBalance(sender, balance))}
+
+*MAIN MENU*${petik}
+ •🌲 ${prefix}menu
+ •🌲 ${prefix}infobot
+ •🌲 ${prefix}donate
+ •🌲 ${prefix}dashboard
+ •🌲 ${prefix}owner
+ •🌲 ${prefix}cekdrive
+ •🌲 ${prefix}cekbandwidth
+ •🌲 ${prefix}cekpremium
+ •🌲 ${prefix}listpremium
+ •🌲 ${prefix}listsewa
+ •🌲 ${prefix}speed
+ •🌲 ${prefix}runtime
+ •🌲 ${prefix}listbahasa${petik}
+
+*CONVERTER/TOOLS*${petik} 
+ •🌱 ${prefix}sticker
+ •🌱 ${prefix}stickerwm
+ •🌱 ${prefix}smeme
+ •🌱 ${prefix}toimg
+ •🌱 ${prefix}tovideo
+ •🌱 ${prefix}tomp3
+ •🌱 ${prefix}ttp
+ •🌱 ${prefix}attp
+ •🌱 ${prefix}emojimix
+ •🌱 ${prefix}nuliskiri
+ •🌱 ${prefix}nuliskanan
+ •🌱 ${prefix}foliokiri
+ •🌱 ${prefix}foliokanan
+ •🌱 ${prefix}say
+ •🌱 ${prefix}translate${petik}
+
+*ANONYMOUS CHATS*${petik}
+ •☘️ ${prefix}anonymous
+ •☘️ ${prefix}start
+ •☘️ ${prefix}next
+ •☘️ ${prefix}stop
+ •☘️ ${prefix}sendprofile${petik}
+
+*STORE MENU*${petik}
+ •💮 ${prefix}list
+ •💮 ${prefix}addlist
+ •💮 ${prefix}dellist
+ •💮 ${prefix}update
+ •💮 ${prefix}jeda
+ •💮 ${prefix}tambah
+ •💮 ${prefix}kurang
+ •💮 ${prefix}kali
+ •💮 ${prefix}bagi
+ •💮 proses < reply chat >
+ •💮 done < reply chat >${petik}
+
+*DOWNLOADS MENU*${petik}
+ •🌀 ${prefix}play
+ •🌀 ${prefix}ytmp3
+ •🌀 ${prefix}ytmp4
+ •🌀 ${prefix}instagram
+ •🌀 ${prefix}igstory
+ •🌀 ${prefix}tiktok
+ •🌀 ${prefix}tiktoknowm
+ •🌀 ${prefix}tiktokaudio
+ •🌀 ${prefix}facebook
+ •🌀 ${prefix}mediafire${petik}
+
+*GROUP MENU*${petik}
+ •🌬️ ${prefix}afk
+ •🌬️ ${prefix}welcome
+ •🌬️ ${prefix}left
+ •🌬️ ${prefix}setwelcome
+ •🌬️ ${prefix}changewelcome
+ •🌬️ ${prefix}delsetwelcome
+ •🌬️ ${prefix}setleft
+ •🌬️ ${prefix}changeleft
+ •🌬️ ${prefix}delsetleft
+ •🌬️ ${prefix}linkgc
+ •🌬️ ${prefix}setppgc
+ •🌬️ ${prefix}setnamegc
+ •🌬️ ${prefix}setdesc
+ •🌬️ ${prefix}antilink
+ •🌬️ ${prefix}antiwame
+ •🌬️ ${prefix}open
+ •🌬️ ${prefix}close
+ •🌬️ ${prefix}add
+ •🌬️ ${prefix}kick
+ •🌬️ ${prefix}promote
+ •🌬️ ${prefix}demote
+ •🌬️ ${prefix}revoke
+ •🌬️ ${prefix}hidetag
+ •🌬️ ${prefix}checksewa${petik}
+
+*GAME MENU*${petik}
+ •❄️ ${prefix}tictactoe
+ •❄️ ${prefix}delttt
+ •❄️ ${prefix}casino
+ •❄️ ${prefix}delcasino${petik}
+
+*SEARCH MENU*${petik}
+ •🌪️ ${prefix}lirik
+ •🌪️ ${prefix}grupwa
+ •🌪️ ${prefix}pinterest
+ •🌪️ ${prefix}ytsearch
+ •🌪️ ${prefix}searchbyimage${petik}
+
+*RANDOM MENU*${petik}
+ •☃️ ${prefix}cecan
+ •☃️ ${prefix}cogan
+ •☃️ ${prefix}waifu${petik}
+
+
+*BALANCE MENU*${petik}
+ •🥶 ${prefix}topglobal
+ •🥶 ${prefix}toplocal
+ •🥶 ${prefix}buylimit
+ •🥶 ${prefix}buyglimit
+ •🥶 ${prefix}transfer
+ •🥶 ${prefix}limit
+ •🥶 ${prefix}balance${petik}
+
+*BAILEYS*${petik}
+ •🐥 ${prefix}fitnah
+ •🐥 ${prefix}nowa
+ •🐥 ${prefix}getquoted
+ •🐥 ${prefix}fakehidetag
+ •🐥 ${prefix}react
+ •🐥 ${prefix}setcmd
+ •🐥 ${prefix}delcmd${petik}
+
+*OWNERS MENU*${petik}
+ •✨ ${prefix}join
+ •✨ ${prefix}left
+ •✨ ${prefix}self
+ •✨ ${prefix}public
+ •✨ ${prefix}setprefix
+ •✨ ${prefix}setppbot
+ •✨ ${prefix}broadcast
+ •✨ ${prefix}bcsewa
+ •✨ ${prefix}addpremium
+ •✨ ${prefix}delpremium
+ •✨ ${prefix}addsewa
+ •✨ ${prefix}delsewa${petik}`
+}
+
+exports.donate = (pushname, ownerNumber) => {
+    return`Contact  Owner:
+wa.me/6283167581315 (Owner)`
+}
